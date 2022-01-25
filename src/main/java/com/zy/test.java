ce17f35e3ba6c234997c721740575c675c5a1790
zy <1792751023@163.com>
2022-01-25 16:16:41 +0800

    first commit

---
+++ src/main/java/com/zy/test.java
@@ -0,0 +1,11 @@
+package com.zy;
+
+/**
+ * @author zy
+ * @create 2022-01-25 16:15
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!!!!");
+    }
+}
